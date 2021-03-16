e40f2cbfb55207d60e0fa34161f8e885a300c021
seanjib99 <78782873+seanjib99@users.noreply.github.com>
2021-03-16 16:42:03 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#2.write a program that roads ehe length
+#height of a
+#right angled triangle
+#and prints the area  every number is given on a separate line
+
+length=int(input("enter the value of length:"))
+height=int(input("enter  the  height: "))
+area=(length*height)//2
+print(f"the area of right angled triangle is {area}")
+
+
